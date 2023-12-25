a07f3370905293daf449e3c5fb9416a47cfb4304
spiderT <1304126527@qq.com>
2023-12-25 17:31:02 +0800

    axum websocket example

---
+++ examples/lists/src/lib.rs
@@ -0,0 +1,44 @@
+pub struct List {
+    head: Link,
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: self.head.take(),
+        });
+
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match self.head.take() {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut boxed_node) = cur_link {
+            cur_link = boxed_node.next.take();
+        }
+    }
+}
